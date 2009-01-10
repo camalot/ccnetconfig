8f7a673645bd12b4a262e20163ca09b1617f2dae
camalot_cp <SND\camalot_cp>
2009-01-10 16:20:42 -0600

    Fixed serializer<T> for required items ([workitem:20552])

--- CCNetConfig.CCNet/SourceControl/MultiSourceControl.cs
+++ CCNetConfig.CCNet/SourceControl/MultiSourceControl.cs
@@ -27,151 +27,156 @@ using System.Drawing.Design;
 using System.Collections;
 
 namespace CCNetConfig.CCNet {
-  /// <summary>
-  /// You can use the 'Multi' Source Control plugin to check for modifications from any number of source control repositories. You may want to 
-  /// do this if (for example) you want to build if the source for your project changes, or if the binaries your project depends on change (which 
-  /// may be stored on a file server).
-  /// </summary>
-  [ MinimumVersion( "1.0" ) ]
-  public class MultiSourceControl : SourceControl, ICCNetDocumentation {
-    private List<SourceControl> _sourceControls;
-    private bool? _requireChangesFromAll = null;
-    /// <summary>
-    /// Initializes a new instance of the <see cref="MultiSourceControl"/> class.
-    /// </summary>
-    public MultiSourceControl ()
-      : base ( "multi" ) {
-      _sourceControls = new List<SourceControl> ();
-    }
-    /// <summary>
-    /// You can use the 'Multi' Source Control plugin to check for modifications from any number of source control repositories. 
-    /// You may want to do this if (for example) you want to build if the source for your project changes, or if the binaries your 
-    /// project depends on change (which may be stored on a file server).
-    /// </summary>
-    /// <value>The source controls.</value>
-    [Description ( "You can use the 'Multi' Source Control plugin to check for modifications from any number of source control repositories. " +
-      "You may want to do this if (for example) you want to build if the source for your project changes, or if the binaries your project depends " +
-      "on change (which may be stored on a file server)." ),
-    Editor ( typeof ( SourceControlListUIEditor ), typeof ( UITypeEditor ) ), TypeConverter ( typeof ( IListTypeConverter ) ),
-   DefaultValue ( null ), DisplayName ( "(SourceControls)" ), Category ( "Required" )]
-    public List<SourceControl> SourceControls { get { return this._sourceControls; } set { this._sourceControls = value; } }
+	/// <summary>
+	/// You can use the 'Multi' Source Control plugin to check for modifications from any number of source control repositories. You may want to 
+	/// do this if (for example) you want to build if the source for your project changes, or if the binaries your project depends on change (which 
+	/// may be stored on a file server).
+	/// </summary>
+	[MinimumVersion ( "1.0" )]
+	public class MultiSourceControl : SourceControl, ICCNetDocumentation {
+		private List<SourceControl> _sourceControls;
+		private bool? _requireChangesFromAll = null;
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MultiSourceControl"/> class.
+		/// </summary>
+		public MultiSourceControl ()
+			: base ( "multi" ) {
+			_sourceControls = new List<SourceControl> ();
+		}
+		/// <summary>
+		/// You can use the 'Multi' Source Control plugin to check for modifications from any number of source control repositories. 
+		/// You may want to do this if (for example) you want to build if the source for your project changes, or if the binaries your 
+		/// project depends on change (which may be stored on a file server).
+		/// </summary>
+		/// <value>The source controls.</value>
+		[Description ( "You can use the 'Multi' Source Control plugin to check for modifications from any number of source control repositories. " +
+			"You may want to do this if (for example) you want to build if the source for your project changes, or if the binaries your project depends " +
+			"on change (which may be stored on a file server)." ),
+		Editor ( typeof ( SourceControlListUIEditor ), typeof ( UITypeEditor ) ), TypeConverter ( typeof ( IListTypeConverter ) ),
+	 DefaultValue ( null ), DisplayName ( "(SourceControls)" ), Category ( "Required" )]
+		public List<SourceControl> SourceControls { get { return this._sourceControls; } set { this._sourceControls = value; } }
 
-    /// <summary>
-    ///If true, only return a list of modifications if all sourceControl sections return a non-empty list. Note that this 
-    /// is short-circuiting, i.e. if the first sourceControl returns an empty list, the next won't be called (this can be useful for situations where 
-    /// you have a slow source control server and you want to check a specific file first as a trigger)
-    /// </summary>
-    /// <value>The require changes from all.</value>
-    [Description ( "If true, only return a list of modifications if all sourceControl sections return a non-empty list. Note that this " +
-      "is short-circuiting, i.e. if the first sourceControl returns an empty list, the next won't be called (this can be useful for situations where " +
-      "you have a slow source control server and you want to check a specific file first as a trigger)" ), DefaultValue ( null ),
-   Editor ( typeof ( DefaultableBooleanUIEditor ), typeof ( UITypeEditor ) ), TypeConverter ( typeof ( DefaultableBooleanTypeConverter ) ), 
-    Category ( "Optional" )]
-    public bool? RequireChangesFromAll { get { return this._requireChangesFromAll; } set { this._requireChangesFromAll = value; } }
+		/// <summary>
+		///If true, only return a list of modifications if all sourceControl sections return a non-empty list. Note that this 
+		/// is short-circuiting, i.e. if the first sourceControl returns an empty list, the next won't be called (this can be useful for situations where 
+		/// you have a slow source control server and you want to check a specific file first as a trigger)
+		/// </summary>
+		/// <value>The require changes from all.</value>
+		[Description ( "If true, only return a list of modifications if all sourceControl sections return a non-empty list. Note that this " +
+			"is short-circuiting, i.e. if the first sourceControl returns an empty list, the next won't be called (this can be useful for situations where " +
+			"you have a slow source control server and you want to check a specific file first as a trigger)" ), DefaultValue ( null ),
+	 Editor ( typeof ( DefaultableBooleanUIEditor ), typeof ( UITypeEditor ) ), TypeConverter ( typeof ( DefaultableBooleanTypeConverter ) ),
+		Category ( "Optional" )]
+		public bool? RequireChangesFromAll { get { return this._requireChangesFromAll; } set { this._requireChangesFromAll = value; } }
 
-    /// <summary>
-    /// Creates a copy of this object.
-    /// </summary>
-    /// <returns></returns>
-    public override SourceControl Clone () {
-      MultiSourceControl msc = this.MemberwiseClone () as MultiSourceControl;
-      msc.SourceControls = new List<SourceControl> ();
-      SourceControl[  ] sc= new SourceControl[ this.SourceControls.Count ];
-      this.SourceControls.CopyTo ( sc );
-      msc.SourceControls.AddRange ( sc );
-      return msc;
-    }
+		/// <summary>
+		/// Creates a copy of this object.
+		/// </summary>
+		/// <returns></returns>
+		public override SourceControl Clone () {
+			MultiSourceControl msc = this.MemberwiseClone () as MultiSourceControl;
+			msc.SourceControls = new List<SourceControl> ();
+			SourceControl[  ] sc= new SourceControl[ this.SourceControls.Count ];
+			this.SourceControls.CopyTo ( sc );
+			msc.SourceControls.AddRange ( sc );
+			return msc;
+		}
 
-    
 
-    /// <summary>
-    /// Serializes this instance.
-    /// </summary>
-    /// <returns></returns>
-    public override System.Xml.XmlElement Serialize () {
-      XmlDocument doc = new XmlDocument ();
-      XmlElement root = doc.CreateElement ( "sourcecontrol" );
-      root.SetAttribute ( "type", this.TypeName );
-      //root.SetAttribute ( "ccnetconfigType", string.Format ( "{0}, {1}", this.GetType ().FullName, this.GetType ().Assembly.GetName ().Name ) );
 
-      XmlElement scs = doc.CreateElement ( "sourceControls" );
-      foreach ( SourceControl scItem in this._sourceControls ) {
-        XmlElement sele = this.ConvertSourceControlToElement ( doc, scItem );
-        if ( sele != null )
-          scs.AppendChild ( sele );
-      }
-      root.AppendChild ( scs );
-      return root;
-    }
+		/// <summary>
+		/// Serializes this instance.
+		/// </summary>
+		/// <returns></returns>
+		public override System.Xml.XmlElement Serialize () {
+			XmlDocument doc = new XmlDocument ();
+			XmlElement root = doc.CreateElement ( "sourcecontrol" );
+			root.SetAttribute ( "type", this.TypeName );
+			//root.SetAttribute ( "ccnetconfigType", string.Format ( "{0}, {1}", this.GetType ().FullName, this.GetType ().Assembly.GetName ().Name ) );
 
-    /// <summary>
-    /// Deserializes the specified element.
-    /// </summary>
-    /// <param name="element">The element.</param>
-    public override void Deserialize ( System.Xml.XmlElement element ) {
-      SourceControls = new List<SourceControl> ();
+			XmlElement scs = doc.CreateElement ( "sourceControls" );
+			foreach ( SourceControl scItem in this._sourceControls ) {
+				XmlElement sele = this.ConvertSourceControlToElement ( doc, scItem );
+				if ( sele != null )
+					scs.AppendChild ( sele );
+			}
+			root.AppendChild ( scs );
+			return root;
+		}
 
-      XmlElement scs = (XmlElement)element.SelectSingleNode ( "sourceControls" );
-      if ( scs != null ) {
-        foreach ( XmlElement tele in scs.SelectNodes ( "./*" ) ) {
-          SourceControl scItem = this.ConvertElementToSourceControl(tele);
-          if ( scItem != null )
-            this._sourceControls.Add ( scItem );
-        }
-      }
-      string s = Util.GetElementOrAttributeValue("requireChangesFromAll",element);
-      if ( !string.IsNullOrEmpty ( s ) )
-        this.RequireChangesFromAll = string.Compare ( s, bool.TrueString, true ) == 0;
-    }
+		/// <summary>
+		/// Deserializes the specified element.
+		/// </summary>
+		/// <param name="element">The element.</param>
+		public override void Deserialize ( System.Xml.XmlElement element ) {
+			SourceControls = new List<SourceControl> ();
 
-    /// <summary>
-    /// Converts the source control to element.
-    /// </summary>
-    /// <param name="owner">The owner.</param>
-    /// <param name="sc">The sc.</param>
-    /// <returns></returns>
-    private XmlElement ConvertSourceControlToElement ( XmlDocument owner, SourceControl sc ) {
-      XmlElement ele = sc.Serialize ();
-      XmlElement tNew = owner.CreateElement ( sc.TypeName );
-      //tNew.SetAttribute ( "ccnetconfigType", string.Format ( "{0}, {1}", sc.GetType ().FullName, sc.GetType ().Assembly.GetName ().Name ) );
-      XmlElement tImport = (XmlElement)owner.ImportNode ( ele, true );
-      foreach ( XmlElement tele in ele.SelectNodes ( "./*" ) ) {
-        tNew.AppendChild ( owner.ImportNode ( tele, true ) );
-      }
+			XmlElement scs = (XmlElement)element.SelectSingleNode ( "sourceControls" );
+			if ( scs != null ) {
+				foreach ( XmlElement tele in scs.SelectNodes ( "./*" ) ) {
+					SourceControl scItem = this.ConvertElementToSourceControl ( tele );
+					if ( scItem != null )
+						this._sourceControls.Add ( scItem );
+				}
+			}
+			string s = Util.GetElementOrAttributeValue ( "requireChangesFromAll", element );
+			if ( !string.IsNullOrEmpty ( s ) )
+				this.RequireChangesFromAll = string.Compare ( s, bool.TrueString, true ) == 0;
+		}
 
-      return tNew;
-    }
+		/// <summary>
+		/// Converts the source control to element.
+		/// </summary>
+		/// <param name="owner">The owner.</param>
+		/// <param name="sc">The sc.</param>
+		/// <returns></returns>
+		/// <workitems>
+		///		<workitem rel="16385">Checked if the node is null before attempting to import it.</workitem>
+		/// </workitems>
+		private XmlElement ConvertSourceControlToElement ( XmlDocument owner, SourceControl sc ) {
+			XmlElement ele = sc.Serialize ();
+			XmlElement tNew = owner.CreateElement ( sc.TypeName );
+			//tNew.SetAttribute ( "ccnetconfigType", string.Format ( "{0}, {1}", sc.GetType ().FullName, sc.GetType ().Assembly.GetName ().Name ) );
+			XmlElement tImport = (XmlElement)owner.ImportNode ( ele, true );
+			foreach ( XmlElement tele in ele.SelectNodes ( "./*" ) ) {
+				XmlNode n = owner.ImportNode ( tele, true );
+				if ( n != null )
+					tNew.AppendChild ( n );
+			}
 
-    /// <summary>
-    /// Converts the element to source control.
-    /// </summary>
-    /// <param name="esc">The esc.</param>
-    /// <returns></returns>
-    private SourceControl ConvertElementToSourceControl ( XmlElement esc ) {
-      XmlElement newEle = esc.OwnerDocument.CreateElement ( "sourcecontrol" );
-      newEle.SetAttribute ( "type", esc.Name );
-      //if ( !string.IsNullOrEmpty ( esc.GetAttribute ( "ccnetconfigType" ) ) )
-        //newEle.SetAttribute ( "ccnetconfigType", esc.GetAttribute ( "ccnetconfigType" ) );
+			return tNew;
+		}
 
-      foreach ( XmlElement ele in esc.SelectNodes ( "./*" ) ) {
-        XmlElement clone = (XmlElement)ele.Clone ();
-        newEle.AppendChild ( clone );
-      }
+		/// <summary>
+		/// Converts the element to source control.
+		/// </summary>
+		/// <param name="esc">The esc.</param>
+		/// <returns></returns>
+		private SourceControl ConvertElementToSourceControl ( XmlElement esc ) {
+			XmlElement newEle = esc.OwnerDocument.CreateElement ( "sourcecontrol" );
+			newEle.SetAttribute ( "type", esc.Name );
+			//if ( !string.IsNullOrEmpty ( esc.GetAttribute ( "ccnetconfigType" ) ) )
+			//newEle.SetAttribute ( "ccnetconfigType", esc.GetAttribute ( "ccnetconfigType" ) );
 
-      SourceControl sc = Util.GetSourceControlFromElement ( newEle );
-      return sc;
-    }
+			foreach ( XmlElement ele in esc.SelectNodes ( "./*" ) ) {
+				XmlElement clone = (XmlElement)ele.Clone ();
+				newEle.AppendChild ( clone );
+			}
 
-    #region ICCNetDocumentation Members
-    /// <summary>
-    /// Gets the documentation URI.
-    /// </summary>
-    /// <value>The documentation URI.</value>
-    [Browsable ( false )]
-    public Uri DocumentationUri {
-      get { return new Uri ( "http://confluence.public.thoughtworks.org/display/CCNET/Multi+Source+Control+Block?decorator=printable" ); }
-    }
+			SourceControl sc = Util.GetSourceControlFromElement ( newEle );
+			return sc;
+		}
 
-    #endregion
-  }
+		#region ICCNetDocumentation Members
+		/// <summary>
+		/// Gets the documentation URI.
+		/// </summary>
+		/// <value>The documentation URI.</value>
+		[Browsable ( false ), ReflectorIgnore ]
+		public Uri DocumentationUri {
+			get { return new Uri ( "http://confluence.public.thoughtworks.org/display/CCNET/Multi+Source+Control+Block?decorator=printable" ); }
+		}
+
+		#endregion
+	}
 }
--- CCNetConfig.Core/Serialization/Serializer.cs
+++ CCNetConfig.Core/Serialization/Serializer.cs
@@ -23,6 +23,7 @@ using System.Xml;
 using CCNetConfig.Core.Components;
 using System.Collections;
 using CCNetConfig.Exceptions;
+using System.Collections.Generic;
 
 namespace CCNetConfig.Core.Serialization {
 	/// <summary>
@@ -59,7 +60,6 @@ namespace CCNetConfig.Core.Serialization {
 					Version maxVer = Util.GetMaximumVersion ( pi );
 					Version exactVer = Util.GetExactVersion ( pi );
 					if ( ignore || ( !Util.IsExactVersion ( exactVer, versionInfo ) && !Util.IsInVersionRange ( minVer, maxVer, versionInfo ) ) ) {
-						Console.WriteLine ( "Ignoring : {0}", pi.Name );
 						continue;
 					}
 					// get node name
@@ -98,69 +98,70 @@ namespace CCNetConfig.Core.Serialization {
 							break;
 						case ReflectorNodeTypes.Element:
 							node = doc.CreateElement ( name );
+
 							if ( required ) {
 								// checks if null or empty...
 								if ( val == null || val.ToString () == string.Empty )
 									throw new RequiredAttributeException ( obj, name );
-								node.InnerText = Util.CheckRequired ( obj, name, val ).ToString ();
-							} else {
-								if ( val != null ) {
-									Type valType = val.GetType ();
-									if ( valType.IsPrimitive || valType == typeof ( string ) ) {
-										node.InnerText = val.ToString ();
-									} else {
-										// handle clonable lists
-										if ( valType.IsGenericType && valType.GetGenericTypeDefinition ().Equals ( typeof ( CloneableList<> ) ) ) {
-											System.Collections.IList vlist = val as IList;
-											foreach ( object o in vlist ) {
-												if ( o is ICCNetObject ) {
-													XmlNode tn = ( (ICCNetObject)o ).Serialize ();
-													if ( tn != null )
-														node.AppendChild ( doc.ImportNode ( tn, true ) );
-												} else {
-													Type o1 = o.GetType ();
-													if ( o1.IsSerializable && o1.IsClass ) {
-														try {
-															string arrayItemName = Util.GetReflectorArrayAttributeValue ( pi );
+							}
 
-															XmlElement tn = doc.CreateElement ( arrayItemName );
-															tn.InnerText = o.ToString ();
-															if ( tn != null )
-																node.AppendChild ( doc.ImportNode ( tn, true ) );
-														} catch {
-															try {
-																string ss = Util.GetStringSeparatorAttributeValue ( pi );
-																XmlElement ele = node as XmlElement;
-																ele.InnerText += string.Format ( "{0}{1}", o.ToString (), ss );
-															} catch { }
-														}
+							if ( val != null ) {
+								Type valType = val.GetType ();
+								if ( valType.IsPrimitive || valType == typeof ( string ) ) {
+									node.InnerText = val.ToString ();
+								} else {
+									// handle clonable lists
+									if ( valType.IsGenericType && valType.GetGenericTypeDefinition ().Equals ( typeof ( CloneableList<> ) ) ) {
+										System.Collections.IList vlist = val as IList;
+										foreach ( object o in vlist ) {
+											if ( o is ICCNetObject ) {
+												XmlNode tn = ( (ICCNetObject)o ).Serialize ();
+												if ( tn != null )
+													node.AppendChild ( doc.ImportNode ( tn, true ) );
+											} else {
+												Type o1 = o.GetType ();
+												if ( o1.IsSerializable && o1.IsClass ) {
+													try {
+														string arrayItemName = Util.GetReflectorArrayAttributeValue ( pi );
+
+														XmlElement tn = doc.CreateElement ( arrayItemName );
+														tn.InnerText = o.ToString ();
+														if ( tn != null )
+															node.AppendChild ( doc.ImportNode ( tn, true ) );
+													} catch {
+														try {
+															string ss = Util.GetStringSeparatorAttributeValue ( pi );
+															XmlElement ele = node as XmlElement;
+															ele.InnerText += string.Format ( "{0}{1}", o.ToString (), ss );
+														} catch { }
 													}
 												}
 											}
-										} else if ( val is HiddenPassword ) { // handle the hidden password object
-											node.InnerText = ( val as HiddenPassword ).GetPassword ();
-										} else if ( valType.GetInterface ( typeof ( ICCNetObject ).FullName ) != null ) { // handle other ICCNetObjects
-											XmlNode tn = ( (ICCNetObject)val ).Serialize ();
-											if ( tn != null ) {
-												// ignore the node that the object creates, use the one the property creates.
-												// then import all the attributes and child nodes
-												foreach ( XmlAttribute attr in tn.Attributes ) {
-													node.Attributes.Append ( doc.ImportNode ( attr, true ) as XmlAttribute );
-												}
-												foreach ( XmlElement ele in tn.SelectNodes ( "./*" ) ) {
-													node.AppendChild ( doc.ImportNode ( ele, true ) );
-												}
+										}
+									} else if ( val is HiddenPassword ) { // handle the hidden password object
+										node.InnerText = ( val as HiddenPassword ).GetPassword ();
+									} else if ( valType.GetInterface ( typeof ( ICCNetObject ).FullName ) != null ) { // handle other ICCNetObjects
+										XmlNode tn = ( (ICCNetObject)val ).Serialize ();
+										if ( tn != null ) {
+											// ignore the node that the object creates, use the one the property creates.
+											// then import all the attributes and child nodes
+											foreach ( XmlAttribute attr in tn.Attributes ) {
+												node.Attributes.Append ( doc.ImportNode ( attr, true ) as XmlAttribute );
 											}
-										} else { // eveything else
-											string formatString = Util.GetFormatAttributeValue ( pi );
-											if ( valType.GetInterface ( typeof ( IFormattable ).FullName ) != null && !string.IsNullOrEmpty ( formatString ) ) {
-												node.InnerText = ( (IFormattable)val ).ToString ( formatString, null );
-											} else {
-												node.InnerText = val.ToString ();
+											foreach ( XmlElement ele in tn.SelectNodes ( "./*" ) ) {
+												node.AppendChild ( doc.ImportNode ( ele, true ) );
 											}
+										}
+									} else { // eveything else
+										string formatString = Util.GetFormatAttributeValue ( pi );
+										if ( valType.GetInterface ( typeof ( IFormattable ).FullName ) != null && !string.IsNullOrEmpty ( formatString ) ) {
+											node.InnerText = ( (IFormattable)val ).ToString ( formatString, null );
+										} else {
+											node.InnerText = val.ToString ();
 										}
 									}
 								}
+
 							}
 							// if it should be added, add it...
 							if ( node != null && ( ( Util.IsNullable ( pi.PropertyType ) && val != null ) || !Util.IsNullable ( pi.PropertyType ) && val != null && !string.IsNullOrEmpty ( val.ToString () ) ) )
@@ -186,18 +187,55 @@ namespace CCNetConfig.Core.Serialization {
 		/// <param name="baseObject">The base object.</param>
 
 		public void Deserialize ( XmlElement element, T baseObject ) {
+			// this dynamically resets all the reflector properties only if they
+			// are writeable and public
 			Util.ResetObjectProperties<T> ( baseObject );
 			Type type = baseObject.GetType ();
 
+			// get the reflector name for the type
 			string rootTypeName = Util.GetReflectorNameAttributeValue ( type );
+			// get the version info for the type
 			Version versionInfo = Util.GetTypeDescriptionProviderVersion ( typeof ( T ) );
 
+			// throw exception if we can not convert from the element to the type.
 			if ( string.Compare ( element.Name, rootTypeName, false ) != 0 )
 				throw new InvalidCastException ( string.Format ( "Unable to convert {0} to a {1}", element.Name, rootTypeName ) );
 
-			foreach ( XmlNode n in element.SelectNodes("*") ) {
+			List<PropertyInfo> props = GetReflectorProperies ( baseObject );
 
+			foreach ( PropertyInfo pi in props ) {
+				string rname = Util.GetReflectorNameAttributeValue ( pi );
+				if ( pi.PropertyType.IsPrimitive || pi.PropertyType == typeof ( string ) ) {
+
+				} else {
+
+				}
 			}
+		}
+
+		/// <summary>
+		/// Gets a list of properties for the specified object that contain a reflector name attribute,
+		/// do not have a reflector ignore attribute and are publicly readable and writeable.
+		/// </summary>
+		/// <param name="obj">The obj.</param>
+		/// <returns></returns>
+		public List<PropertyInfo> GetReflectorProperies ( T obj ) {
+			List<PropertyInfo> props = new List<PropertyInfo> ();
+			Type objType = obj.GetType ();
+			PropertyInfo [] propArray = objType.GetProperties ( BindingFlags.Instance | BindingFlags.Public );
+			foreach ( PropertyInfo pi in propArray ) {
+				// see if the property has a reflector name attribute
+				ReflectorNameAttribute rna = Util.GetCustomAttribute<ReflectorNameAttribute> ( pi );
+				// make sure it doesn't have a reflector ignore attribute
+				ReflectorIgnoreAttribute ria = Util.GetCustomAttribute<ReflectorIgnoreAttribute> ( pi );
+				// we also need to check that both the get and set methods of the property are public.
+				bool canRead = pi.CanRead && pi.GetGetMethod ( false ) != null;
+				bool canWrite = pi.CanRead && pi.GetSetMethod ( false ) != null;
+				if ( rna != null && ria == null && canRead && canWrite ) {
+					props.Add ( pi );
+				}
+			}
+			return props;
 		}
 
 		/*public void Deserialize ( System.Xml.XmlElement element, T baseObject ) {
